406612fb18f6f001a8cc0c88ff55c12e0631764b
kvchahar <68537060+kvchahar@users.noreply.github.com>
2020-10-29 23:22:25 +0530

    Update Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,19 @@
-package thread.tortoise;
+/*   Created by IntelliJ IDEA.
+ *   Author: Krishnaveer Chahar
+ *   Date: 29-10-2020
+ *   Time: 18:30
+ *   File: Tortoise.java
+ */
+package main;
 
-public class Tortoise {
+import thread.rabbit.Rabbit;
+import thread.tortoise.Tortoise;
+
+public class Race {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
+    }
 }
